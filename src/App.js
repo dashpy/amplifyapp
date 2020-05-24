ad4b626d9ae71514fdc70768311523bc55ff2ea3
dashpy <fdra2323@gmail.com>
2020-05-24 14:26:15 -0400

    Add unmaintain message

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "shards-ui/dist/css/shards.min.css"
 import './App.css';
 import Footer from './footer';
+import Header from './Header';
+
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
+        <Header/>
         <Body/>
         <Footer/>
       </div>
---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import './footer.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div >
+         <p>
+          Page without mantaince. Check my LinkedIn Profile for further information.
+         </p>
+      </div>
+    );
+  }
+}
+
+export default Header;
